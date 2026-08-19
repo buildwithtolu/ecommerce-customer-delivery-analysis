82dcada6318156e2955485e02cc8debd73b37fe8
Tolulope Olatunji <kafkadsa@gmail.com>
2026-08-19 18:13:42 +0100

    Create customer_analytics table in schema

--- sql/01_schema_setup.sql
+++ sql/01_schema_setup.sql
@@ -1,1 +1,15 @@
+CREATE TABLE customer_analytics(
+			 ID	int,
+			 Warehouse_block VARCHAR(10),	
+			 Mode_of_Shipment VARCHAR(20),
+			 Customer_care_calls int,
+			 Customer_rating int,
+			 Cost_of_the_Product int,
+			 Prior_purchases int,
+			 Product_importance	VARCHAR(10),
+			 Gender VARCHAR(10),
+			 Discount_offered int,
+			 Weight_in_gms int,
+			 Reached_on_Time int
+			 );
 
